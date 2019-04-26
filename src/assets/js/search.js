71516a73daf50f7fc30efecab0bd0163480b2fb2
Mkomarr <m.koomarr@gmail.com>
2019-04-26 10:52:30 +0200

    Zadanie wykonane

---
+++ src/assets/js/search.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('.search-input');
+
+const search = () => {
+
+     if (input === "") {
+          return;
+     } else {
+          console.log('skrypt wyszukiwarki');
+     }
+};
